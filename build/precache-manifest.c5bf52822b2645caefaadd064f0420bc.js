e0f236602a93fa67878f169ec72ce603f8337ecd
Deepak /kumar /singh <deepakkumarsingh@Deepaks-MacBook-Pro.local>
2022-10-22 22:58:24 +0530

    build

---
+++ build/precache-manifest.c5bf52822b2645caefaadd064f0420bc.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f45eed4c94f0b9676e177b5a8c73c627",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8743b9b6aaaf740c9c17",
+    "url": "/static/css/2.678ef759.chunk.css"
+  },
+  {
+    "revision": "9ae2d9c9cf4520b4b87a",
+    "url": "/static/css/main.b5a3db21.chunk.css"
+  },
+  {
+    "revision": "8743b9b6aaaf740c9c17",
+    "url": "/static/js/2.5a9b1973.chunk.js"
+  },
+  {
+    "revision": "8c70df15ed1b75400346b9d2c3d29848",
+    "url": "/static/js/2.5a9b1973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ae2d9c9cf4520b4b87a",
+    "url": "/static/js/main.0cd83370.chunk.js"
+  },
+  {
+    "revision": "e16ab5b4c7161de1ec9b",
+    "url": "/static/js/runtime-main.8bacd91a.js"
+  },
+  {
+    "revision": "a190967e810471db9ef67a4b07a99b09",
+    "url": "/static/media/Image 1.a190967e.png"
+  },
+  {
+    "revision": "1be3401f3b0cea86129d17ba64f829d9",
+    "url": "/static/media/LOGO.1be3401f.svg"
+  },
+  {
+    "revision": "41cea9a3f02ef2a3cc01c31a51e265ba",
+    "url": "/static/media/addDevice.41cea9a3.png"
+  },
+  {
+    "revision": "1445166d1b4f7b1bc2fafaeee525f926",
+    "url": "/static/media/certified_img.1445166d.png"
+  },
+  {
+    "revision": "e48835a22d5cd3a2a0262951c29591f1",
+    "url": "/static/media/crop_management_image.e48835a2.png"
+  },
+  {
+    "revision": "3f7dc55b6b2dda03cf3430b601a1466e",
+    "url": "/static/media/farmer.3f7dc55b.svg"
+  },
+  {
+    "revision": "28350b96c95897f989f3bd8fa6dfe61b",
+    "url": "/static/media/fieldCorrectPic.28350b96.svg"
+  },
+  {
+    "revision": "d1ef82cbad3600249e958233bb6d9db3",
+    "url": "/static/media/iconedit.d1ef82cb.svg"
+  },
+  {
+    "revision": "cef63efe2fc9bfde74eb79b5e1a7254a",
+    "url": "/static/media/iconlocation.cef63efe.svg"
+  },
+  {
+    "revision": "0f3f26f2c9dfa8ea2a427f608cd1812e",
+    "url": "/static/media/icontoday.0f3f26f2.svg"
+  },
+  {
+    "revision": "4ac7d4cffb286c12adf452a41c999ddc",
+    "url": "/static/media/sensorsImage.4ac7d4cf.png"
+  },
+  {
+    "revision": "fd5728f2cf777b06b966d05c0c823dc9",
+    "url": "/static/media/spritesheet.fd5728f2.svg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
